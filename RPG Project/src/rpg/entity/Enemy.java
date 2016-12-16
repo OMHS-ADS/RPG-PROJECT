7802f0ee58ce034cd276312ef295dffcb973ea46
clarencesmom <yomamaisgoinhate@gmail.com>
2016-12-16 14:03:14 -0500

    yes i copied it from character

--- RPG Project/src/rpg/entity/Enemy.java
+++ RPG Project/src/rpg/entity/Enemy.java
@@ -1,5 +1,13 @@
 package rpg.entity;
 //kyle sluss
 public abstract class Enemy {
-
+private double health;
+private int x;
+private int y;
+public void moveX(int X){
+	x = X;
+}
+public void moveY(int Y){
+	y = Y;
+}
 }
