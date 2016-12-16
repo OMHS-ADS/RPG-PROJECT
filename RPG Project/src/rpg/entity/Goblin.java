7ceb094d8a4dca732a58ae353db1057f1c477b15
Morgan <quixoticbibliomania@gmail.com>
2016-12-16 09:48:13 -0500

    Added stub classes for characters

---
+++ RPG Project/src/rpg/entity/Goblin.java
@@ -0,0 +1,62 @@
+package rpg.entity;
+
+import rpg.item.Shield;
+import rpg.item.Weapon;
+
+public class Goblin extends Character {
+
+	@Override
+	public void move(int direction) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pickup(String item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void drop(String item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void attack(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void attack(String name, Weapon w) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void defend() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void defend(Shield s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getBagContents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
