038ee2bad164f00ce228fa84bbf98faabd1752a9
jfreem9916 <jonahwfreeman@yahoo.com>
2016-12-22 11:01:29 -0500

    hi

---
+++ RPG Project/src/rpg/World.java
@@ -0,0 +1,18 @@
+package rpg;
+
+public class World {
+	
+	private Tile[][] foreground;
+	private Tile[][] background;
+	
+	public World(){
+		System.out.println("Hello world!");
+	}
+	public void setBGTile(Tile t, int r, int c){
+		background[r][c] = t;
+	}
+	public void setFGTile(Tile t, int r, int c){
+		foreground[r][c] = t;
+	}
+	public 
+}
