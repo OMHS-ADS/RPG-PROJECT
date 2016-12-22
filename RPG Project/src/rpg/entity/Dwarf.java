14e04450d28023a4fea217df7087de4af4dd6fc3
cnikie8165 <cnikie8165@OMHB45C4289D22.inst.hcpss.org>
2016-12-22 10:12:38 -0500

    Character things

--- RPG Project/src/rpg/entity/Dwarf.java
+++ RPG Project/src/rpg/entity/Dwarf.java
@@ -9,37 +9,33 @@ public class Dwarf extends PlayerCharacter {
 	@Override
 	public void attack(PlayerCharacter name) {
 		// TODO Auto-generated method stub
-
+		//do 1 dmg default
+		//default attack value, dmg = attack * modifier
+		// if hits(true){
+		// name.defend(dmg * defend multiplier (smaller than with shield))
+		//}
+		//name.defend(name.getShield()){
+		//}
 	}
 
 	@Override
 	public void attack(PlayerCharacter name, Weapon w) {
 		// TODO Auto-generated method stub
-
+		//do 1 dmg default
+		//default attack value, dmg = attack * modifier
+		//do more dmgs based on modifier
 	}
 
 	@Override
 	public void defend() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void defend(Shield s) {
 		// TODO Auto-generated method stub
 
-	}
-
-	@Override
-	public String getBagContents() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getLocation() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
