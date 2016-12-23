5ca6f4ed4481cb05f262cf63d16c67712e4fe9d1
Morgan <Morgan@MORGAN-PC>
2016-12-22 20:51:20 -0500

    Cut out variables covered by ItemEntity and PlayerCharacter
    See comments in Item line 1 & 2.

--- RPG Project/src/rpg/item/Item.java
+++ RPG Project/src/rpg/item/Item.java
@@ -1,60 +1,62 @@
+//equipped & state are covered by PlayerCharacter
+//xPos/yPos are covered by ItemEntity
 package rpg.item;
 abstract public class Item {
     protected int MAP; //maximum attack points the item can increase a character's attack by
     protected int DHP; //maximum defense points the item can increase a character's defense
     protected String name;  //the name of the item
-    protected boolean state; // 0 if on the ground, 1 if in character's bag
-    protected int xPos, yPos;
-    protected boolean equipped; //0 if not in use, 1 if in use
+    //protected boolean state; // 0 if on the ground, 1 if in character's bag
+    //protected int xPos, yPos;
+    //protected boolean equipped; //0 if not in use, 1 if in use
+    
     
     public Item() {
-        state = false;
-        equipped = false;
+        //state = false;
+        //equipped = false;
     }
     
     
-    public Item(String nameIn, int x, int y){
-        xPos = x;
+    public Item(String nameIn){
+        //xPos = x;
         name = nameIn;
-        yPos = y;
+        //yPos = y;
       //  MAP = MAPin;
        // DHP = DHPin;
-        equipped = false;
-        state = false;
+        //equipped = false;
+        //state = false;
     }
     
     //add other constructor(s) here if necessary
     
-    public String toString(){
-        String str = name + " : \n"
-                   + "Equipped = " + equipped + "\n"
-                   + "In somebody's inventory = " + state + "\n"
-                   + "(X,Y) position = (" + xPos + ", " + yPos + ")";
+    public String toString(){ 
+        String str = name;
+        		   // + " : \n"
+                   //+ "Equipped = " + equipped + "\n"
+                   //+ "In somebody's inventory = " + state + "\n"
+                   //+ "(X,Y) position = (" + xPos + ", " + yPos + ")";
         return str;
     }
-                        
-    public void setX(int x){
-        xPos = x;
-    }
+
+    @Deprecated                  
+    public void setX(int x){}
+
+    @Deprecated
+    public void setY(int y){}
+
+    @Deprecated
+    public void setEq(boolean eq){}
+
+    @Deprecated
+    public void setState(boolean inin){}
     
-    public void setY(int y){
-        yPos = y;
-    }
-    
-    public void setEq(boolean eq){
-        equipped = eq;
-    }
-    
-    public void setState(boolean inin){
-        state = inin;
-    }
-    
+    @Deprecated
     public boolean getState(){
-        return state;
+        return false;
     }
-    
+
+    @Deprecated
     public boolean getEq(){
-        return equipped;
+        return false;
     }
     public int getMAP(){
     	return MAP;
