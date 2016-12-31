f73d5ebad2d6348c48bce1873c51293cd3d6ccce
xDest | Zach <destroyer7712@gmail.com>
2016-12-31 04:48:33 -0500

    Updated Game and GameFrame

--- RPG Project/src/rpg/Game.java
+++ RPG Project/src/rpg/Game.java
@@ -6,15 +6,51 @@ import rpg.graphics.GameFrame;
 
 public class Game {
 
+	private World currentWorld;
+	
+	//Nice steady 16 fps
 	public static void main(String[] args) {
-		GameFrame title = new GameFrame();
-		ArrayList<World> worlds = new ArrayList<World>();
-		worlds.add(new World());
-
+		Game g = new Game();
+		g.start();
 	}
 	
-	public void update(World w) {
-		
-		
+	
+	
+	
+	
+	public Game() {
+		currentWorld = World.getWorld(0);
 	}
+	
+	public void start() {
+		GameFrame title = new GameFrame();
+		title.setVisible(true);
+		int TICK = 16;
+		final int nsPerTick = (int) 1e9 / TICK;
+		double timePassed = 0;
+		double currentTime = System.nanoTime();
+		double lastTime = System.nanoTime();
+
+		double nsPassed = 0;
+		while (title.isVisible()) {
+			currentTime = System.nanoTime();
+			nsPassed += currentTime - lastTime;
+			timePassed += (currentTime - lastTime) / nsPerTick;
+
+			if (timePassed >= 1) {
+				// Tick!!!
+				title.renderWorld(this);
+				timePassed -= 1;
+			}
+
+			lastTime = currentTime;
+		}
+	}
+	// ArrayList<World> worlds = new ArrayList<World>();
+	// worlds.add(new World());
+	
+	public World getCurrentWorld() {
+		return currentWorld;
+	}
+
 }
--- RPG Project/src/rpg/graphics/GameFrame.java
+++ RPG Project/src/rpg/graphics/GameFrame.java
@@ -1,26 +1,54 @@
 package rpg.graphics;
 
+import java.awt.image.BufferedImage;
+
 import javax.swing.JFrame;
 
+import rpg.Game;
+import rpg.Tile;
 import rpg.World;
 
 public class GameFrame extends JFrame {
 	
+	
 	public GameFrame() {
 		super("World of Datacraft");
-		setSize(16*60, 9*60 + 20);
+		setSize(16*60 + 1, 9*60 + 23);
 		setVisible(true);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
 	
-	
-	
-	public void updateFrame(World w) {
-		//Do update frame
-		
+	public void renderWorld(Game g) {
+		BufferedImage bi = new BufferedImage((int)this.getSize().getWidth(),(int)this.getSize().getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+		World w = g.getCurrentWorld();
+		Tile[][] bg = w.getBG();
+		Tile[][] fg = w.getFG();
+		int yoffset = 22;
+		//Draw bg
+		for (int r = 0; r < bg.length; r++) {
+			for(int c = 0; c < bg[c].length; c++) {
+				bg[r][c].getTileEntity().render(bi.getGraphics(), r*Tile.TILE_SIZE, c*Tile.TILE_SIZE + yoffset);
+			}
+		}
+		//Draw grid
+		for (int y = 0; y <= bg[0].length; y++) {
+			 bi.getGraphics().drawLine(0, (y*Tile.TILE_SIZE)+yoffset,  (int)this.getSize().getWidth(), (y * Tile.TILE_SIZE) + yoffset);
+		}
+		for (int x = 0; x < bg.length; x++) {
+			 bi.getGraphics().drawLine((x * Tile.TILE_SIZE), 0+yoffset,  (x * Tile.TILE_SIZE), (int)this.getSize().getHeight());
+		}
+		//Draw fg
+		for (int r = 0; r < fg.length; r++) {
+			for(int c = 0; c < fg[c].length; c++) {
+				fg[r][c].getTileEntity().render(bi.getGraphics(), r*Tile.TILE_SIZE, c*Tile.TILE_SIZE + yoffset);
+			}
+		}
+		this.getGraphics().drawImage(bi, 0, 0, null);
 		
 	}
+
+	
 
 }
