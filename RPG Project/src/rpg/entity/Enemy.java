e291a1f19c672075b0b0d335122b1040c73d72d4
clarencesmom <yomamaisgoinhate@gmail.com>
2017-01-02 10:01:29 -0500

    usgcgh vcAE

--- RPG Project/src/rpg/entity/Enemy.java
+++ RPG Project/src/rpg/entity/Enemy.java
@@ -9,18 +9,17 @@ public abstract class Enemy {
 		protected int MDP; //maximum defense points of the character excluding other items
 		protected int HP; //current health points a character has
 		protected String name;  //the name of the character
-<<<<<<< HEAD
 		protected int xPos, yPos; //location of the character
 		protected boolean boss;
-=======
->>>>>>> refs/remotes/origin/master
+		protected float hitChance = (float) 0.75, baseDefend = 0.1f;
+
 		
 	    public abstract void move(int direction);   //  			(move in a direction multiple space(s)
 	    //public abstract void pickup(String item);	//		(pickup a visible item)
-	    public abstract void drop(String item);			//(drop an item at your current location)
+	    //public abstract void drop(String item);			//(drop an item at your current location)
 	    public abstract void attack(String name);		//	(attack another character)
-	    public abstract void attack(String name, Weapon w);	//(attack a character with an item)
+	    //public abstract void attack(String name, Weapon w);	//(attack a character with an item)
 	    public abstract void defend();				//(defend an attack)
-	    public abstract void defend(Shield s);	//		(defend an attack with an item)
+	    //public abstract void defend(Shield s);	//		(defend an attack with an item)
 	    public abstract String getLocation();			//	(return�s the current character�s room location as X, Y)
 }
