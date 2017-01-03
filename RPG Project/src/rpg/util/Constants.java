e5c85e268a1e4acda346f700fde54feffb7a64bc
Morgan <quixoticbibliomania@gmail.com>
2017-01-03 08:57:45 -0600

    Added constants and put an error check on move

---
+++ RPG Project/src/rpg/util/Constants.java
@@ -0,0 +1,6 @@
+package rpg.util;
+
+public class Constants {
+	public final static int WORLDMAX_X=16; //Number of tiles in x direction
+	public final static int WORLDMAX_Y=9; //Number of tiles in y direction
+}
