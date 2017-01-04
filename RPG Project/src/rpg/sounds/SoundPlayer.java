53a377c29cd2dc1bd0f30aed4ce5860924d9cc7e
ptian9707 <ptian9707@OMHB45C4247C16.inst.hcpss.org>
2017-01-04 11:29:22 -0500

    more sounds bruh

--- RPG Project/src/rpg/sounds/SoundPlayer.java
+++ RPG Project/src/rpg/sounds/SoundPlayer.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import javax.sound.sampled.LineEvent.Type;
 
 public class SoundPlayer {
-	
+	/*attack magic
+	enemy defeat
+	damage enemy
+	take damage
+	*/
 	public SoundPlayer(){
 		//empty
 	}
@@ -34,6 +38,18 @@ public class SoundPlayer {
 	    }
 	}
 	
+	public void playMissSound(){
+		try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("missSound.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace(); //remove later
+	    }
+	}
+	
 	public void playMagicSound(){
 		try {
 	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("magicSound.wav").getAbsoluteFile());
@@ -49,6 +65,18 @@ public class SoundPlayer {
 	public void playBlockSound(){
 		try {
 	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("blockSound.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace(); //remove later
+	    }
+	}
+	
+	public void playPickupSound(){
+		try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("pickupSound.wav").getAbsoluteFile());
 	        Clip clip = AudioSystem.getClip();
 	        clip.open(audioInputStream);
 	        clip.start();
